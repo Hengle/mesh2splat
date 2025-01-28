bf8927fa83030922907b62f161fd0a1bb340c213
sscolari <sscolari@ea.com>
2025-01-28 09:48:05 +0100

    Var name change in splatting VS shader

--- src/shaders/rendering/gaussianSplattingVS.glsl
+++ src/shaders/rendering/gaussianSplattingVS.glsl
@@ -89,33 +89,27 @@ void main() {
 	computeCov3D(gaussianQuaternion, exp(gaussianPackedScale.xyz) * u_std_dev, cov3d);
 	vec4 gaussian_vs = u_worldToView * vec4(gaussianPosition_ms.xyz, 1);
 
-	vec4 pos2dHom = u_viewToClip * gaussian_vs;
-	vec4 tmp = pos2dHom;
-	//persective divide
-	pos2dHom.xyz = pos2dHom.xyz / pos2dHom.w;
-	pos2dHom.w = 1.f;
+	vec4 pos2d = u_viewToClip * gaussian_vs;
+	pos2d.xyz = pos2d.xyz / pos2d.w;
+	pos2d.w = 1.f;
 
-	vec2 wh = 2 * u_hfov_focal.xy * u_hfov_focal.z;
+	vec2 wh = 2 * u_hfov_focal.xy * (u_hfov_focal.z);
 	float limx = 1.3 * u_hfov_focal.x;
 	float limy = 1.3 * u_hfov_focal.y;
 
-	//To NDC with persp div
+	//projected screen-space coordinates
 	float txtz = gaussian_vs.x / gaussian_vs.z;
 	float tytz = gaussian_vs.y / gaussian_vs.z;
 
 	float tx = min(limx, max(-limx, txtz)) * gaussian_vs.z;
 	float ty = min(limy, max(-limy, tytz)) * gaussian_vs.z; 
 
-	//if (any(greaterThan(abs(pos2dHom.xyz), vec3(1.3)))) {
-	//	gl_Position = vec4(2, 2, 0, 1);
-	//	return;	
-	//}
-
 	//Jacobian of affine approximation of projective transformation
+	//We just care about how z affects, u_hfov_focal.xy is already baked into the projection mat
 	mat3 J = mat3(
 	  u_hfov_focal.z / gaussian_vs.z, 0., 0,
-	  -(u_hfov_focal.z * tx) / (gaussian_vs.z * gaussian_vs.z), u_hfov_focal.z / gaussian_vs.z, 0,
-	  0., -(u_hfov_focal.z * ty) / (gaussian_vs.z * gaussian_vs.z), 0.
+	  -(u_hfov_focal.x * tx) / (gaussian_vs.z * gaussian_vs.z), u_hfov_focal.z / gaussian_vs.z, 0,
+	  0., -(u_hfov_focal.y * ty) / (gaussian_vs.z * gaussian_vs.z), 0.
 	);
 	
 	mat3 JW = J * mat3(u_worldToView);
@@ -132,11 +126,12 @@ void main() {
 	float det_inv = 1.f / det;
 
 	vec2 quadwh_scr = vec2(3.f * sqrt(cov2d[0][0]), 3.f * sqrt(cov2d[1][1]));
-	vec2 quadwh_ndc = (quadwh_scr / wh) * 2; //HMM
-	pos2dHom.xy = pos2dHom.xy + vertexPos.xy * quadwh_ndc;
+	//from screen to NDC, inverse
+	vec2 quadwh_ndc = (quadwh_scr / wh) * 2;
+	pos2d.xy = pos2d.xy + vertexPos.xy * quadwh_ndc;
 
-	gl_Position = pos2dHom;
-	//TODO: Should actually use the conic to compute varying densit
+	gl_Position = pos2d;
+	//TODO: Should actually use the conic to compute varying density
 	out_conic = vec3(cov2d[1][1] * det_inv, -cov2d[0][1] * det_inv, cov2d[0][0] * det_inv);
 	out_color = gaussianColor.rgb;
 	out_opacity = gaussianColor.a;
